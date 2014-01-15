87bacea08450da801d6cb498e9d82091718d1f06
fab <cristian@cristian-945GCT-M.(none)>
2014-01-15 01:06:31 -0500

    correccion de error de graficacion en la impresion de columnas, no se ha corregido el problema de tamaño

--- js/solicitud.js
+++ js/solicitud.js
@@ -1,19 +1,56 @@
 
+var bar =null;
+
 /** solicitud
 *Esta funcion se encarga de realizar la solicitud al servidor
 */
 function solicitud(frase){
-	$.get(frase,carga);
-}
+	//contenedor que contendra la informacion filtrada y organizada
+	var contenedor = new Array();
+	//consulta y asignacion
+	contenedor=filtro(frase.datas);
+	console.log(contenedor);
+	//el largo total de años a graficar en un solo dato
+	var largo_subllave = contenedor[0].length;
+	//cantidad total de datos a plasmar en el eje x
+	var cant_datos = contenedor.length;
 
+	var aux;
+	for(i=cant_datos-1;i>=0;i--){
+		for(j=largo_subllave-1;j>=0;j--){
+			//console.log(i,j,contenedor[i][j]);
+			aux = contenedor[i][j][1];
+			carga(aux,asig_color(contenedor[i][j][0]),cant_datos,i);
+		}
+	}
+	//$.get(frase,carga);
+}
+function cargar_bar(lienzo,data,pos_dato,barWidth){
+	if(bar!=null){
+		bar=bar.data([data])
+	    	.enter().append("g")
+	    	.attr("transform", function(d) {return "translate(" + pos_dato * barWidth + ",0)"; });
+		return bar;
+	}
+	else{
+		bar = lienzo.selectAll("g")
+	    	.data([data])
+	    	.enter().append("g")
+	    	.attr("transform", function(d) {return "translate(" + pos_dato * barWidth + ",0)"; });
+	   	return bar;
+	}
+	
+}
 /** carga
 * Esta funcion se encarga de graficar los datos enviados a través del parametro
 * data.
 */
-function carga(data,color){
+function carga(data,color,cant_datos,pos_dato){
 //Tamaño del lienzo	
+	//console.log(data, color,cant_datos,pos_dato);
+
 	var width = 960,
-	    height = 500;
+    	height = 500;
 
 	var y = d3.scale.linear()
 	    .range([height, 0]);
@@ -25,24 +62,23 @@ function carga(data,color){
 	//asignacion de la informacion requerida
 	//data = data.datas[2008];
 	//Se separan las llaves y los datos
-	var llaves = Object.keys(data);
+	/*var llaves = Object.keys(data);
 	var claves= new Array;
 	for(var i=0;i<llaves.length;i++){
 		claves[i]=data[llaves[i]];
-	}
+	}*/
 	//--------------------- GRAFICACION ---------------------------
 	//dominio de la grafica
-	y.domain([0, d3.max(claves, function(d) { return d; })]);
-	var barWidth = width / Object.keys(data).length;
+	y.domain([0, cant_datos/*d3.max(data, function(d) { return d; })*/]);
+	var barWidth = width / /*Object.keys(data).length;*/cant_datos;
+	console.log('barWidth: '+barWidth);
+	cargar_bar(lienzo,data,pos_dato,barWidth);
 	
-	var bar = lienzo.selectAll("g")
-	    .data(claves)
-	    .enter().append("g")
-	    .attr("transform", function(d, i) {return "translate(" + i * barWidth + ",0)"; });
+	console.log('bar: '+bar);
 	bar.append("rect")
 	    .attr("class", color)
-	    .attr("y", function(d) { var y1= y(d);return y1; })
-	    .attr("height", function(d) { return height - y(d); })
+	    .attr("y", function(d) {var n = y(d);console.log('n: '+n); return n; })
+	    .attr("height", function(d) { var x=height-y(d);console.log('x: '+x);return x; })
 	    .attr("width", barWidth - 1);
 	 
 	bar.append("text")
@@ -101,7 +137,6 @@ function filtro(data){
 function ordenar(dato){
 	var tam = dato.length;
 	var temp,temp2,j;
-	console.log(dato);
 	for(var i = 0;i<tam;i++){
 		temp = dato[i][1];
 		temp2 = dato[i];
@@ -113,4 +148,37 @@ function ordenar(dato){
 	return dato;
 }
 
+/** color
+* Esta funcion se encarga de asignar el color deseado a cada dato
+*/
+function asig_color(anio){
+	var color_r= "negro";
+	switch(anio){
+		case '2006': 
+					color_r = "rojo";
+					break;
+		case '2007':
+					color_r = "azul";
+					break;
+		case '2008':
+					color_r = "verde";
+					break;
+		case '2009':
+					color_r = "amarillo";
+					break;
+		case '2010':
+					color_r = "negro";
+					break;
+		case '2011':
+					color_r = "morado";
+					break;
+		case '2012':
+					color_r = "rosa";
+					break;
+		default:
+					color_r="negro";
+					break;
+	}
+	return color_r;
 
+}
